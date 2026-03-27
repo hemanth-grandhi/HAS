8ee4e20cc589a30f6d9da7c48bdc46216a3f223b
saranprithvick <prithvicksaran11@gmail.com>
2026-03-27 20:08:28 +0530

    Added BillingController and CheckInController

---
+++ has-backend/src/main/java/com/has/backend/controller/BillingController.java
@@ -0,0 +1,28 @@
+package com.has.backend.controller;
+
+import com.has.backend.entity.Bill;
+import com.has.backend.service.BillingService;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/billing")
+public class BillingController {
+    private final BillingService service;
+
+    public BillingController(BillingService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/checkout/{tokenNumber}")
+    public Bill processCheckout(@PathVariable String tokenNumber) {
+        return service.processCheckout(tokenNumber);
+    }
+
+    @PostMapping("/confirm-payment/{billId}")
+    public void confirmPayment(@PathVariable Long billId) {
+        service.confirmPayment(billId);
+    }
+}
